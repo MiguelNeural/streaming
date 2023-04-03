cf0e2a7a402332b81073d276d5c68a9b7f643810
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-03 11:23:10 -0600

    Actualización del modelo de camaras

--- cameras_admin/models.py
+++ cameras_admin/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Camera(models.Model):
+    camera_id = models.IntegerField(primary_key=True)
     name = models.TextField()
     rtsp = models.TextField()
+    peop_c_service = models.BooleanField(default=False, verbose_name='People Count Service')
+    face_rec_service = models.BooleanField(default=False, verbose_name='Face Recognition Service')
+    vehicules_service = models.BooleanField(default=False, verbose_name='Vehicles Detection Service')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     deleted = models.DateTimeField(null=True, blank=True)
@@ -12,3 +16,4 @@ class Camera(models.Model):
         db_table = 'cameras'
         managed = False
     using = 'neuralDB'
+    
