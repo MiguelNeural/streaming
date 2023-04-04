5432394c2fb99b1ccf777eff33d2890e67a79d3e
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-04 12:02:48 -0600

    Valida valores del formulario para agregar camara. Envía nombre y id de la camara al formulario de eliminar camara.

--- cameras_admin/static/cameras_admin/js/cameras.js
+++ cameras_admin/static/cameras_admin/js/cameras.js
@@ -1,11 +1,51 @@
 $(document).ready(function () {
 	$("#saveCamera_btn").click(function (e) {
 		e.preventDefault();
-		$("#createCamera_form").submit();
+		form = $("#createCamera_form");
+		if (form[0].checkValidity()) {
+			form.submit();
+		} else {
+			form.each(function () {
+				let inputs = $("#createCamera_form :input");
+				inputs.each(function () {
+					if (!this.validity.valid) {
+						$(this)
+							.next(".invalid-feedback")
+							.text(this.validationMessage)
+							.addClass("d-block");
+					} else {
+						$(this)
+							.next(".invalid-feedback")
+							.removeClass("d-block");
+					}
+				});
+			});
+		}
 	});
 
-	$("#sendRTSP_btn").click(function (e) {
+	$('input[type="checkbox"]').click(function () {
+		$('input[type="checkbox"]').not(this).prop('checked', false);
+	});
+
+	$("#watchCameraForm_btn").click(function (e) {
 		e.preventDefault();
-		$("#sendCamera_form").submit();
+		$("#watchCamera_form").submit();
+	});
+
+	// $("#cameraFormModal").on("show.coreui.modal", function (event) {
+	// 	let button = $(event.relatedTarget);
+	// 	button.each(function () {
+	// 		if (this.outerText == "edit") {
+	// 			$("#cameraId_field").val(button.data("camera-id"));
+	// 		} else {
+	// 			$("#cameraId_field").val(null);
+	// 		}
+	// 	});
+	// });
+
+	$("#deleteCameraModal").on("show.coreui.modal", function (event) {
+		let button = $(event.relatedTarget);
+		$("#deleteCameraId_input").val(button.data("camera-id"));
+		$("#camera_name_div").text(button.data("camera-name"));
 	});
 });
