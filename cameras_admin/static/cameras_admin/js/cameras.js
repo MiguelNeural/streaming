bf144cf4b929bd8c08b10ae046ee1f43bc905646
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-10 11:17:31 -0600

    lóogica para llenar el formulario de editar cámara

--- cameras_admin/static/cameras_admin/js/cameras.js
+++ cameras_admin/static/cameras_admin/js/cameras.js
@@ -39,9 +39,29 @@ $(document).ready(function () {
 	});
 
 	function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
+	
 	if (isNumber(this.URL.split('/')[4])) {
-		console.log("Camera founded:", cameraById_json);
+		const cameraJson = JSON.parse($("#cameraJSON").val())[0];
+		const peopleCount = $("#editPeopleCount_checkbox");
+		const faceRec = $("#editFaceRec_checkbox");
+		const vehicles = $("#editVehicules_checkbox");
+
+		$("#id_field").val(cameraJson.pk);
+		$("#editName_field").val(cameraJson.fields.name);
+		$("#editRtsp_field").val(cameraJson.fields.rtsp);
+		
+		console.log(cameraJson.fields)
+
+		if(cameraJson.fields.peop_c_service) {
+			peopleCount.attr('checked', true);
+		}
+		if(cameraJson.fields.face_rec_service) {
+			faceRec.attr('checked', true);
+		}
+		if(cameraJson.fields.vehicules_service) {
+			vehicles.attr('checked', true);
+		}
+
 		$("#editCameraModal").modal("show");
-		$("#id_field").val()
 	}
 });
