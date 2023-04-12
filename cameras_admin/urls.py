da686511e47a6ee9ced886339ca30c9bc8d5151b
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-12 13:42:05 -0600

    Mueve urls de la aplicacion camaras al url.py de cameras_admin

---
+++ cameras_admin/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from cameras_admin import views
+
+urlpatterns = [
+    path('', views.cameras, name='cameras'),
+    path('created/', views.create_camera, name='create_camera'),
+    path('<id>/', views.edit_camera, name='edit_camera'),
+    path('delete/<id>/', views.delete_camera, name='delete_camera'),
+    path('rtsp/<id>', views.rtsp_camera, name='rtsp_camera'),
+]
--- streaming/urls.py
+++ streaming/urls.py
@@ -14,21 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from dashboard import views as dash_views
-from cameras_admin import views as cam_views
+from cameras_admin.views import video_feed
 
 urlpatterns = [
     path('', dash_views.blank, name='blank'),
     path('dashboard/', dash_views.index, name="dashboard"),
-    
-    path('cameras/', cam_views.cameras, name='cameras'),
-    path('cameras/created/', cam_views.create_camera, name='create_camera'),
-    path('cameras/<id>/', cam_views.edit_camera, name='edit_camera'),
-    path('cameras/delete/<id>/', cam_views.delete_camera, name='delete_camera'),
-    path('cameras/rtsp/<id>', cam_views.rtsp_camera, name='rtsp_camera'),
-    
-    path('video_feed/', cam_views.video_feed, name='video_feed'),
+    path('cameras/', include('cameras_admin.urls')),
+    path('video_feed/', video_feed, name='video_feed'),
     
     path('admin/', admin.site.urls),
 ]
