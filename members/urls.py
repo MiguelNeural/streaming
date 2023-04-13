095ce4a5149bae98d36c35cd45b8e7781a77f3f7
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-13 12:54:49 -0600

    Agrega url para ir al access point de crear usuario

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,5 @@ from members import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('admin/', views.members, name='members'),
-    
+    path('created/', views.create_member, name='create_member')
 ]
