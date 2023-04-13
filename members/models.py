ec16c740c057fad45e5dc0d0299067fed9a50481
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-13 09:43:59 -0600

    Crea el modelos de la tabla de usuarios

--- members/models.py
+++ members/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    member_id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    password = models.TextField()
+    role = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        app_label = 'members'
+        db_table = 'members'
+        managed = False
