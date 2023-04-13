a4c38723d7cf99f859c39fa56bb16a2d340406b7
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-13 16:51:31 -0600

    Agrega urls para crear y editar usuarios

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,6 @@ from members import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('admin/', views.members, name='members'),
-    path('created/', views.create_member, name='create_member')
+    path('created/', views.create_member, name='create_member'),
+    path('<id>/', views.edit_member, name='edit_member'),
 ]
