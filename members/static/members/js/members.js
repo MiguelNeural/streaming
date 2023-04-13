21d598f988fb1e26245674a28bb7805ff40be40f
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-13 16:52:03 -0600

    Agrega funcion para abrir, llenar y validar formulario de edicion de usuarios

--- members/static/members/js/members.js
+++ members/static/members/js/members.js
@@ -21,4 +21,37 @@ $(document).ready(function () {
             });
         }
     });
+    
+    function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
+	
+	if (isNumber(this.URL.split('/')[4]) && this.URL.split('/')[3]=='members') {
+        const memberJson = JSON.parse($("#memberJSON").val())[0];
+        console.log(memberJson.fields.role);
+		$("#id_field").val(memberJson.pk);
+		$("#editName_field").val(memberJson.fields.name);
+        $("#editPassword_field").val(memberJson.fields.password);
+        $('#editRole_field option[value="' + memberJson.fields.role + '"]').prop('selected', true);
+
+		$("#editMemberModal").modal("show");
+    }
+    
+    $("#editMember_btn").click(function (e) {
+		e.preventDefault();
+		form = $("#editMember_form");
+		if (form[0].checkValidity()) {
+			form.submit();
+		}
+		else {
+			form.each(function () {
+				let inputs = $("#editMember_form :input");
+				inputs.each(function () {
+					if (!this.validity.valid) {
+						$(this).next(".invalid-feedback").text(this.validationMessage).addClass("d-block");
+					} else {
+						$(this).next(".invalid-feedback").removeClass("d-block");
+					}
+				});
+			});
+		}
+	});
 });
