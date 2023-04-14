8ff645772308d45744b719c0a78d90fea4133693
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-14 11:43:15 -0600

    Cambia urls de usuarios a raiz de members/ y agrega login a las urls principales

--- streaming/urls.py
+++ streaming/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from dashboard import views as dash_views
 from cameras_admin.views import video_feed
+from members.views import login
 
 urlpatterns = [
     path('', dash_views.blank, name='blank'),
     path('dashboard/', dash_views.index, name="dashboard"),
+    
     path('cameras/', include('cameras_admin.urls')),
     path('video_feed/', video_feed, name='video_feed'),
+    
     path('members/', include('members.urls')),
+    path('login/', login, name='login'),
     
     path('admin/', admin.site.urls),
 ]
