912f819a4520287ca1223b8c9c8525d557932579
Miguel Marquez <mmarquez@hydra-technologies.com>
2023-04-24 12:44:42 -0600

    comenta nuevas rtsps. hardcode de rtsp para testing.

--- cameras_admin/modules/camera.py
+++ cameras_admin/modules/camera.py
@@ -4,7 +4,10 @@ import threading
 
 class VideoCamera(object):
     def __init__(self, rtsp):
+        # rtsp://root:Aegis4040@192.168.15.103/frstream
         # rtsp://root:Aegis4040@192.168.15.103/mystream
+        # rtsp://root:Aegis4040@192.168.5.35/live.sdp
+        # rtsp://neuralio:Aegis4040@192.168.5.46/live.sdp
         
         # Envíar rtsp al doctor Gehova:
         # address = ('192.168.15.103', 6000)
@@ -20,7 +23,7 @@ class VideoCamera(object):
         #        result = 'No response from server.'
         #        break
         
-        rtsp = 'rtsp://192.168.15.103:8554/mystream'
+        rtsp = 'rtsp://root:invitado@192.168.5.44/live.sdp'
         self.video = cv2.VideoCapture(rtsp)
         (self.grabbed, self.frame) = self.video.read()
         threading.Thread(target=self.update, args=()).start()
